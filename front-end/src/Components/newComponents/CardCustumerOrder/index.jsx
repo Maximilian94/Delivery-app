68b9cb639bd340da7dbe89a30d1bb02bf4b45393
Maximilian Kaden <mkadenfilgo@gmail.com>
2021-09-18 15:33:37 -0300

    refactor: Order Card

--- front-end/src/Components/newComponents/CardCustumerOrder/index.jsx
+++ front-end/src/Components/newComponents/CardCustumerOrder/index.jsx
@@ -4,7 +4,8 @@ import './style.css';
 
 function CardCostumerOrder(selectOrder) {
   const { selectOrder: userOrder } = selectOrder;
-  const { id, status, saleDate, totalPrice } = userOrder;
+  const { id, status, saleDate, totalPrice, deliveryAddress, deliveryNumber } = userOrder;
+  const userType = JSON.parse(localStorage.getItem('user')).role;
 
   function ajustData(data) {
     const limit = 2;
@@ -21,7 +22,7 @@ function CardCostumerOrder(selectOrder) {
   const cardIdentification = () => (
     <div className="order-card-identification">
       Pedido
-      <span data-testid={ `customer_orders__element-order-id-${id}` }>
+      <span data-testid={ `${userType}_orders__element-order-id-${id}` }>
         { id }
       </span>
     </div>
@@ -29,7 +30,7 @@ function CardCostumerOrder(selectOrder) {
 
   const cardStatus = () => (
     <div className="order-card-status pending">
-      <span data-testid={ `customer_orders__element-delivery-status-${id}` }>
+      <span data-testid={ `${userType}_orders__element-delivery-status-${id}` }>
         { status }
       </span>
     </div>
@@ -37,20 +38,27 @@ function CardCostumerOrder(selectOrder) {
 
   const cardDataAndPrice = () => (
     <div className="order-card-data-price">
-      <span data-testid={ `customer_orders__element-order-date-${id}` }>
+      <span data-testid={ `${userType}_orders__element-order-date-${id}` }>
         { ajustData(saleDate) }
       </span>
-      <span data-testid={ `customer_orders__element-card-price-${id}` }>
+      <span data-testid={ `${userType}_orders__element-card-price-${id}` }>
         { `R$ ${ajustPrice(totalPrice)}` }
       </span>
     </div>
   );
 
   return (
-    <Link to={ `/customer/orders/${id}` } className="order-card">
+    <Link to={ `/${userType}/orders/${id}` } className="order-card">
       {cardIdentification()}
-      {cardStatus()}
-      {cardDataAndPrice()}
+      <div className="order-card-information">
+        <div className="order-card-information-up">
+          {cardStatus()}
+          {cardDataAndPrice()}
+        </div>
+        <div className="order-card-address">
+          <span>{ `${deliveryAddress}, ${deliveryNumber}` }</span>
+        </div>
+      </div>
     </Link>
   );
 }
